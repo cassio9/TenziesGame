1ffcad75beb51ca64656e074edad7d7810887975
Cassio Soares <cassiosoares9@hotmail.com>
2022-12-02 15:23:59 +0900

    change layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,106 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Dice from "./components/Dice";
-import Confetti from 'react-confetti'
-
+import Confetti from "react-confetti";
+import { nanoid } from "nanoid";
+import useWindowSize from "./hooks/ConfettiSize";
 
 function App() {
+	const allTimeRecord = Number(localStorage.getItem("RecordTenzies") || 50);
+	//localStorage.removeItem("RecordTenzies");
+	const [randomNumbers, setRandomNumbers] = useState(allNewDice);
+	const [win, setWin] = useState(false);
+	const [count, setCount] = useState(0);
+	const { width, height } = useWindowSize();
 
-  const allTimeRecord = Number(localStorage.getItem("RecordTenzies") || 50)
-  //localStorage.removeItem("RecordTenzies")
-  const [randomNumbers, setRandomNumbers] = useState(JogaDado());
-  const [win, setWin] = useState(false);
-  const [count, setCount] = useState(0)
+	function getNewDice() {
+		return {
+			number: Math.floor(Math.random() * 6 + 1),
+			on: false,
+			id: nanoid(),
+		};
+	}
 
-  function JogaDado() {
-    let Arr = [];
-    for (let i = 0; i < 10; i++) {
-      Arr.push(Math.floor(Math.random() * 6) + 1);
-    }
-    let diceRandomObj = Arr.map((ranNum, index) => {
-      return {
-        number: ranNum,
-        id: index,
-        on: false,
-      };
-    });
-    return diceRandomObj;
-  }
+	function allNewDice() {
+		let newDice = [];
+		for (let i = 0; i < 10; i++) {
+			newDice.push(getNewDice());
+		}
+		return newDice;
+	}
 
-  function rollDice() {
-    setRandomNumbers((prevState) =>
-      prevState.map((dice) => {
-        return dice.on
-          ? dice
-          : {
-              number: Math.floor(Math.random() * 6) + 1,
-              id: Math.floor(Math.random() * 600000) + 1,
-              on: false,
-            };
-      })
-    );
-    setCount(prevState => prevState + 1)
-  }
+	function rollDice() {
+		setRandomNumbers((prevState) =>
+			prevState.map((dice) => {
+				return dice.on ? dice : { ...dice, number: Math.floor(Math.random() * 6) + 1 };
+			})
+		);
+		setCount((prevState) => prevState + 1);
+	}
 
-  function SelectNumber(id) {
-    setRandomNumbers((prevState) =>
-      prevState.map((elem) => {
-        return elem.id === id ? { ...elem, on: !elem.on } : elem;
-      })
-    );
-  }
+	function SelectNumber(id) {
+		setRandomNumbers((prevState) =>
+			prevState.map((elem) => {
+				return elem.id === id ? { ...elem, on: !elem.on } : elem;
+			})
+		);
+	}
 
-  function resetGame() {
-    setRandomNumbers(JogaDado);
-    setWin(false);
-    setCount(0)
-  }
+	function resetGame() {
+		setRandomNumbers(allNewDice);
+		setWin(false);
+		setCount(0);
+	}
 
-  useEffect(() => {
-    const allHeld = randomNumbers.every((die) => die.on);
-    const firstValue = randomNumbers[0].number;
-    const allSameValue = randomNumbers.every(
-      (die) => die.number === firstValue
-    );
-    if (allHeld && allSameValue) {
-      setWin(true);
-      // put delay into this "if statement", in order to our Winner record render the proper value
-      setTimeout(() => {
-	        if (count < allTimeRecord || !allTimeRecord){
-	          localStorage.setItem("RecordTenzies", count)
-	        }
-      }, 100);
-    }
-  }, [randomNumbers]);
+	useEffect(() => {
+		const allHeld = randomNumbers.every((die) => die.on);
+		const firstValue = randomNumbers[0].number;
+		const allSameValue = randomNumbers.every((die) => die.number === firstValue);
+		if (allHeld && allSameValue) {
+			setWin(true);
+			// put delay into this "if statement", in order to our Winner record render the proper value
+			setTimeout(() => {
+				if (count < allTimeRecord || !allTimeRecord) {
+					localStorage.setItem("RecordTenzies", count);
+				}
+			}, 100);
+		}
+		return () => clearTimeout();
+	}, [randomNumbers]);
 
-  let DiceGame = randomNumbers.map((elem) => {
-    return (
-      <Dice
-        num={elem.number}
-        key={elem.id}
-        on={elem.on}
-        StopRoll={() => SelectNumber(elem.id)}
-      />
-    );
-  });
+	let DiceGame = randomNumbers.map((elem) => {
+		return (
+			<Dice num={elem.number} key={elem.id} on={elem.on} StopRoll={() => SelectNumber(elem.id)} />
+		);
+	});
 
-  console.log(count, allTimeRecord)
-  return (
-    <div className="App">
-      {win && <Confetti numberOfPieces={300} />}
-      <h1 className="main-title">Tenzies</h1>
-      <p className="main-p">
-        Roll until all dice are the same. Click each die to freeze it at its
-        current value between rolls.
-      </p>
-      <div className="board-container">{DiceGame}</div>
-      <button
-        className={`btn ${win ? "hidden" : ""}`}
-        id="play-btn"
-        onClick={rollDice}
-      >
-        Roll
-      </button>
-      <button
-        className={`btn-reset ${!win ? "hidden" : ""}`}
-        id="reset-btn"
-        onClick={resetGame}
-      >
-        Reset Game
-      </button>
-      <div
-        className={`score ${!win ? "hidden" : ""}`}
-        id="reset-btn"
-      >
-        <p>{count < allTimeRecord ? "New Record! Congrats" : "Round Score"}</p>
-        <span>{count < allTimeRecord ? `\uD83D\uDE80` : ""} {count}</span>
-        <p>{count < allTimeRecord ? "Previous Record" : "World Record"}</p>
-        <span>{count < allTimeRecord ? "" : `\uD83D\uDE80` } {allTimeRecord || 0}</span>
-      </div>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			{win && <Confetti width={width} height={height} numberOfPieces={500} />}
+			<h1 className="main-title">Tenzies</h1>
+			<p className="main-p">
+				Roll until all dice are the same. Click each die to freeze it at its current value between
+				rolls.
+			</p>
+			<div className="board-container">{DiceGame}</div>
+			<button className={`btn ${win ? "hidden" : ""}`} id="play-btn" onClick={rollDice}>
+				Roll
+			</button>
+			<button className={`btn-reset ${!win ? "hidden" : ""}`} id="reset-btn" onClick={resetGame}>
+				Reset Game
+			</button>
+			<div className={`score ${!win ? "hidden" : ""}`} id="reset-btn">
+				<p>{count < allTimeRecord ? "New Record! Congrats" : "Round Score"}</p>
+				<span>
+					{count < allTimeRecord ? `\uD83D\uDE80` : ""} {count}
+				</span>
+				<p>{count < allTimeRecord ? "Previous Record" : "World Record"}</p>
+				<span>
+					{count < allTimeRecord ? "" : `\uD83D\uDE80`} {allTimeRecord || 0}
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,116 +1,117 @@
 function Dice(props) {
+	// Layout dice HTML
+	const { diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix } = DiceHTMLStructure();
 
-    // Layout dice HTML
-  const { diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix } = DiceHTMLStructure();
+	// Render Dice
+	let { classRender, diceRender } = RenderDice();
 
-  // Render Dice
-  let { classRender, diceRender } = RenderDice();
+	return (
+		<div
+			className={`${classRender}-face dice`}
+			style={{
+				backgroundColor: props.on ? "#2ecc71" : "wheat",
+				border: props.on ? "3px solid #2ecc71" : "3px solid wheat",
+			}}
+			onClick={props.StopRoll}>
+			{diceRender}
+		</div>
+	);
 
-  return (
-    <div
-      className={`${classRender}-face dice`}
-      style={{ backgroundColor: props.on ? "#59E391" : "tomato", border: props.on ? "3px solid black" : "3px solid tomato" }}
-      onClick={props.StopRoll}
-    >
-      {diceRender}
-    </div>
-  );
+	function RenderDice() {
+		let diceRender;
+		let classRender;
+		switch (props.num) {
+			case 1:
+				diceRender = diceOne;
+				classRender = "first";
+				break;
+			case 2:
+				diceRender = diceTwo;
+				classRender = "second";
+				break;
+			case 3:
+				diceRender = diceThree;
+				classRender = "third";
+				break;
+			case 4:
+				diceRender = diceFour;
+				classRender = "fourth";
+				break;
+			case 5:
+				diceRender = diceFive;
+				classRender = "fifth";
+				break;
+			case 6:
+				diceRender = diceSix;
+				classRender = "sixth";
+				break;
+		}
+		return { classRender, diceRender };
+	}
 
-    function RenderDice () {
-        let diceRender;
-        let classRender;
-        switch (props.num) {
-            case 1:
-                diceRender = diceOne;
-                classRender = "first";
-                break;
-            case 2:
-                diceRender = diceTwo;
-                classRender = "second";
-                break;
-            case 3:
-                diceRender = diceThree;
-                classRender = "third";
-                break;
-            case 4:
-                diceRender = diceFour;
-                classRender = "fourth";
-                break;
-            case 5:
-                diceRender = diceFive;
-                classRender = "fifth";
-                break;
-            case 6:
-                diceRender = diceSix;
-                classRender = "sixth";
-                break;
-        }
-        return { classRender, diceRender };
-    }
+	function DiceHTMLStructure() {
+		const diceOne = (
+			<>
+				<span className="dot"></span>
+			</>
+		);
+		const diceTwo = (
+			<>
+				<span className="dot"></span>
+				<span className="dot"></span>
+			</>
+		);
+		const diceThree = (
+			<>
+				<span className="dot"></span>
+				<span className="dot"></span>
+				<span className="dot"></span>
+			</>
+		);
+		const diceFour = (
+			<>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+			</>
+		);
+		const diceFive = (
+			<>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+				<div className="column">
+					<span className="dot"></span>
+				</div>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+			</>
+		);
 
-    function DiceHTMLStructure () {
-        const diceOne = <>
-            <span className="dot"></span>
-        </>;
-        const diceTwo = (
-            <>
-                <span className="dot"></span>
-                <span className="dot"></span>
-            </>
-        );
-        const diceThree = (
-            <>
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-            </>
-        );
-        const diceFour = (
-            <>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-            </>
-        );
-        const diceFive = (
-            <>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-                <div className="column">
-                    <span className="dot"></span>
-                </div>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-            </>
-        );
-
-        const diceSix = (
-            <>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-                <div className="column">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-            </>
-        );
-        return { diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix };
-    }
+		const diceSix = (
+			<>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+				<div className="column">
+					<span className="dot"></span>
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</div>
+			</>
+		);
+		return { diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix };
+	}
 }
 
 export default Dice;
-
-
